fedf1cb3503bb5d4f5c94fa81b30076f2406c428
Florian80 <florianheld80@gmail.com>
2017-05-18 13:52:36 +0200

    Added ngForms to login
    Let's log the shit out of it.

--- miSchnuf/src/pages/home/home.ts
+++ miSchnuf/src/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
 import { HomePatientPage } from '../home-patient/home-patient';
 import { NavController } from 'ionic-angular';
 
@@ -8,15 +9,46 @@ import { NavController } from 'ionic-angular';
 })
 
 
-export class HomePage {
+export class HomePage implements OnInit {
 
-    constructor(public navCtrl: NavController) {
+
+    myForm: FormGroup;
+    userInfo: { name: string, password: string } = { name: '', password: '' };
+
+    constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+
+    }
+
+    ngOnInit(): any {
+
+        this.myForm = this.formBuilder.group({
+            'name': ['', this.nameValidator.bind(this)],
+            'password': ['', this.passwordValidator.bind(this)]
+        });
 
     }
 
     pushPage() {
         // push another page on to the navigation stack
         this.navCtrl.push(HomePatientPage);
+    }
+
+    onSubmit() {
+
+        console.log('submitting form');
+
+    }
+
+    nameValidator(control: FormControl): { [s: string]: boolean } {
+        if (!control.value.match("^[a-zA-Z ,.'-]+$")) {
+            return { invalidName: true };
+        }
+    }
+
+    passwordValidator(control: FormControl): { [s: string]: boolean } {
+        if (!control.value.match("^[a-zA-Z ,.'-]+$")) {
+            return { invalidPassword: true };
+        }
     }
 
 }
